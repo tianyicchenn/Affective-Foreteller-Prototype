4b2687d545a53f5652d132c4f9e22afab9effc2d
tianyicchenn <tianyichen9828@gmail.com>
2021-04-21 14:48:35 +0200

    Initial Commit

---
+++ Affective Foretell 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Affective Foretell 2
+//
+//  Created by Tianyi Chen on 21/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
